3233dd2d20e28dee3c6ff6ac209196595ccb5be3
DESKTOP-TSCA153\Dave <davechezffm@gmail.com>
2022-01-22 12:07:06 +0000

    RESTART

---
+++ CodeBlockersGameJam/Assets/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+   
+    private Snake snake;
+    public bool hit;
+    public bool works;
+    public int destroy;
+    public float destroyCounter;
+   
+    private void Start()
+    {
+        snake = FindObjectOfType<Snake>();
+        destroyCounter = destroy;
+    }
+
+    private void Update()
+    {
+        if (hit)
+        {
+            gameObject.transform.position = snake.tail2.position;
+            destroyCounter -=Time.deltaTime;
+            if (destroyCounter < 0)
+            { Destroy(snake.tail2.GetComponent<GameObject>());
+                destroyCounter=destroy;
+            }
+
+            
+        }
+    }
+
+
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Tail"))
+        {
+            hit = true;
+            works = true;
+        }
+    }
+}
---
+++ CodeBlockersGameJam/Assets/Scripts/Snake.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    private Vector2 direction = Vector2.right;
+    private List<Transform> segmentsList;
+    public Transform segmentPrefab;
+    public int tail;
+    public Transform tail2;
+    public GameObject enemy;
+
+    private void Start()
+    {
+        segmentsList = new List<Transform>();
+        segmentsList.Add(this.transform);
+       
+    }
+
+
+    private void Update()
+    {
+        enemy.GetComponent<Pathfinding.AIDestinationSetter>().target = tail2;
+        tail = segmentsList.Count;
+        tail2 = segmentsList[tail - 1];
+        
+        if (direction.x != 0f) {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                direction = Vector2.up;
+
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                direction = Vector2.down;
+            }
+        }
+
+
+
+        if (direction.y != 0f)
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                direction = Vector2.left;
+
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                direction = Vector2.right;
+
+            }
+        }
+        
+    }
+
+    private void FixedUpdate()
+    { for (int i = segmentsList.Count - 1; i > 0; i--)
+        {
+            segmentsList[i].position = segmentsList[i - 1].position;
+        }
+        transform.position = new Vector3(
+         Mathf.Round(transform.position.x) + direction.x,
+         Mathf.Round(transform.position.y) + direction.y
+         , 0.0f);
+
+
+        for (int i = segmentsList.Count - 1; i > 0; i--)
+        {
+            if (segmentsList[i].position != segmentsList[i - 1].position)
+            {
+                segmentsList[i].tag = "Obstacle";
+
+            }
+            tail2.tag = "Tail";
+                
+        }
+
+            }
+
+    private void Grow()
+    {
+        Transform segment = Instantiate(segmentPrefab);
+        segment.position = segmentsList[segmentsList.Count - 1].position;
+
+            segmentsList.Add(segment);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Food"))
+        {
+            Grow();
+        }
+
+        if (collision.CompareTag ("Obstacle"))
+        {
+            ResetGame();
+        }
+
+    }
+
+    private void ResetGame()
+    {
+        for (int i = 1; i < segmentsList.Count; i++)
+        {
+            Destroy(segmentsList[i].gameObject);
+        }
+            segmentsList.Clear();
+            segmentsList.Add(this.transform);
+            this.transform.position = Vector3.zero;
+        
+    }
+}
