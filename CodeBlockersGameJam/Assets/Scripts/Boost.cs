b3580ce87e9c69207c63b5b46813d7766493bdec
DESKTOP-TSCA153\Dave <davechezffm@gmail.com>
2022-01-22 16:43:01 +0000

    Working Boost Script, With changes implemented in Scene 1

--- CodeBlockersGameJam/Assets/Scripts/Boost.cs
+++ CodeBlockersGameJam/Assets/Scripts/Boost.cs
@@ -4,8 +4,32 @@ using UnityEngine;
 
 public class Boost : MonoBehaviour
 {
+  
+    public int boostSpeedTime = 3;
+    private float boostSpeedTimeCounter;
+    private bool boost;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.comp)
+        if (collision.CompareTag("Player"))
+        {//Here is the number to change how much faster the player goes.
+            Time.fixedDeltaTime = 0.03f;
+            boost = true;
+            boostSpeedTimeCounter = boostSpeedTime;
+
+        }
+    }
+    private void Update()
+    {
+        if (boost)
+        {
+            boostSpeedTimeCounter -= Time.deltaTime;
+            if (boostSpeedTimeCounter < 0)
+            {
+                boost = false;
+                Time.fixedDeltaTime = 0.05f;
+
+
+            }
+        }
     }
 }
